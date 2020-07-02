4be5da707ea2055ac879e1d58fc069eba6f9737f
Adeel <moadeel321@gmail.com>
2020-07-02 20:35:13 +0500

    simple routing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 let fs = require("fs");
 let http = require("http");
 let port = 8080;
+let url = require("url");
 
 let server = http.createServer(handle);
 
@@ -13,23 +14,46 @@ server.listen(port,()=>{
 //api data
 
 let data = fs.readFileSync(`${__dirname}/data/data.json`,"utf-8");
+let dataObject = JSON.parse(data);
+let overview = fs.readFileSync(`${__dirname}/overview-template.html`,"utf-8");
+let cards = fs.readFileSync(`${__dirname}/cards-template.html`,"utf-8");
+let productTemplate = fs.readFileSync(`${__dirname}/product.html`,"utf-8");
 
 
 function handle(req,res){
-    let pathName = req.url;
+    let {query , pathname} = url.parse(req.url,true);
 
-    if(pathName === "/" || pathName === "/overview"){
-        res.end("OVERVIEW PAGES");
+    //Overview page
+    if(pathname === "/" || pathname === "/overview"){
+        res.writeHead(200,{
+            "Content-Type" : "text/html"
+           });
+        let cardsHTML = dataObject.map((element)=> replaceHTML(cards,element)).join("");
+        overview = overview.replace(/{%CARDS%}/g,cardsHTML);  
+        res.end(overview);
     }
-    else if(pathName === "/products"){
-        res.end("PRODUCTS PAGES");
+
+    //Product page
+    else if(pathname === "/products"){
+        res.writeHead(200,{
+            "Content-Type" : "text/html"
+        });
+
+        let product = dataObject[query.id];
+        let output = replaceHTML(productTemplate,product);
+
+        res.end(output);
     }
-    else if(pathName === "/api"){
+
+    //API
+    else if(pathname === "/api"){
        res.writeHead(200,{
         "Content-Type" : "application/json"
        });
        res.end(data);
     }
+
+    //404 page
     else{
         res.writeHead(404,{
             "Content-type" : "text/html"
@@ -38,6 +62,29 @@ function handle(req,res){
     }
 
 }
+
+
+function replaceHTML(temp , el){
+    let output = temp.replace(/{%IMAGE%}/g,el.image);
+    output = output.replace(/{%NAME%}/g,el.productName);
+    output = output.replace(/{%QUANTITY%}/g,el.quantity);
+    output = output.replace(/{%PRICE%}/g,el.price);
+    output = output.replace(/{%ID%}/g,el.id);
+    output = output.replace(/{%DESCRIPTION%}/g,el.description);
+    return output;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 fs.readFile("./data/data.json",(error,content)=>{
