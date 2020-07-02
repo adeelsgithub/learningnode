cbf1b635c881ff917476a6a89e6b65d84f701335
Adeel <moadeel321@gmail.com>
2020-07-02 18:46:04 +0500

    created simple api

--- server.js
+++ server.js
@@ -10,6 +10,44 @@ server.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
 
+//api data
+
+let data = fs.readFileSync(`${__dirname}/data/data.json`,"utf-8");
+
+
 function handle(req,res){
-    res.end("HELLO WORLD");
+    let pathName = req.url;
+
+    if(pathName === "/" || pathName === "/overview"){
+        res.end("OVERVIEW PAGES");
+    }
+    else if(pathName === "/products"){
+        res.end("PRODUCTS PAGES");
+    }
+    else if(pathName === "/api"){
+       res.writeHead(200,{
+        "Content-Type" : "application/json"
+       });
+       res.end(data);
+    }
+    else{
+        res.writeHead(404,{
+            "Content-type" : "text/html"
+        });
+        res.end("<h1>PAGE NOT FOUND</h1>");
+    }
+
 }
+
+/*
+fs.readFile("./data/data.json",(error,content)=>{
+    if(error){
+        console.log("ERROR!!!")
+    }else{
+        res.writeHead(200,{
+            "Content-Type" : "application/json"
+        })
+        res.end(content);
+    }
+});
+*/
